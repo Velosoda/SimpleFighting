4e39fe1b01d8fcb19bbb181176e21969df17a61b
Velosoda <velosoda@kean.edu>
2016-08-21 18:30:10 -0400

    Add files via upload

---
+++ src/unit7problemset/Constants.java
@@ -0,0 +1,6 @@
+package unit7problemset;
+
+public class Constants 
+{
+	 public static final String menu = "Menu \n1:Guy Store";
+}
---
+++ src/unit7problemset/Main.java
@@ -0,0 +1,15 @@
+ package unit7problemset;
+ import java.util.*;
+ public class Main
+ {
+//     String menu = Constants.menu;
+     public void menu()
+     {
+         System.out.println(Constants.menu);
+     }
+     public static void main(String[] args)
+     {
+         Main main = new Main();
+         main.menu();
+     }
+ }
