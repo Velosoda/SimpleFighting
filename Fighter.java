ec7ed752c70e754f3cd5aed6a4fa821da5e29f43
Velosoda <velosoda@kean.edu>
2016-09-06 20:34:29 -0400

    Add files via upload

---
+++ Fighter.java
@@ -0,0 +1,15 @@
+package pack;
+import java.util.*;
+
+public class Fighter
+{
+	Random rand = new Random(); 
+	//data/variables
+	public String name = "";
+	public double maxHealth = 10;
+	public double health = maxHealth;
+	public double attack = 1;
+	public int speed = 1;
+	public String type = "Arms";
+	public double attackMultiplier = 1; 
+}
